9a6f9a50a0abe2f428683ef1e06096f136fcc8c8
supal <supal.iut03@gmail.com>
2020-04-18 09:18:26 +0600

    first commit

---
+++ src/main/java/com/example/myphotobackend/MyphotoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.myphotobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyphotoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyphotoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myphotobackend/MyphotoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myphotobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyphotoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
